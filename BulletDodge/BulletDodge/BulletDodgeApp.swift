ea6d6e076845858ba4314751001c53e3b361f255
kenshin0413 <kenshin1234569@gmail.com>
2026-07-06 15:17:02 +0900

    初期

---
+++ BulletDodge/BulletDodge/BulletDodgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletDodgeApp.swift
+//  BulletDodge
+//
+//  Created by miyamotokenshin on R 8/07/06.
+//
+
+import SwiftUI
+
+@main
+struct BulletDodgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BulletDodge/BulletDodge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BulletDodge
+//
+//  Created by miyamotokenshin on R 8/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
